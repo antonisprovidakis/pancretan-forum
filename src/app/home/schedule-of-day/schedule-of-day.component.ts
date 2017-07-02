2971870268cda236ff4fe2f07b0c752a4ad4c6b3
Dimitris Maramatakis <mitsakos.m@gmail.com>
2017-07-03 01:39:29 +0300

    today's schedule component completed

--- src/app/home/schedule-of-day/schedule-of-day.component.ts
+++ src/app/home/schedule-of-day/schedule-of-day.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleOfDayComponent implements OnInit {
 
+  meetings: any[] = [
+    {
+      meetingWith: 'Antonakis Farms',
+      time: '09:00'
+    },
+    {
+      meetingWith: 'User123 Farms',
+      time: '09:10'
+    },
+    {
+      meetingWith: 'User Farms',
+      time: '09:30'
+    },
+
+
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  attendMeeting() {
+    //TODO: implement what attend button should do.
+    console.log('attended');
+
+  }
+
+  displayAttendButton(meetingTime: string): boolean {
+    let today = new Date();
+    let mTime = new Date();
+
+    let h, min;
+    [h, min] = meetingTime.split(/[:.]/);
+
+    mTime.setHours(parseInt(h));
+    mTime.setMinutes(parseInt(min) + 2);
+    return mTime.getTime() > today.getTime();
   }
 
 }
