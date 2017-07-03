ff43bd7556390aa0701e3ee24435105814b4fe43
Antonios Providakis <ant.providakis@gmail.com>
2017-07-04 00:07:48 +0300

    Added hosting related files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAVOLCcOnaPRDa2_QgRKDqdrk9wuCzWS7c',
+    authDomain: 'pancretan-forum.firebaseapp.com',
+    databaseURL: 'https://pancretan-forum.firebaseio.com',
+    projectId: 'pancretan-forum',
+    storageBucket: 'pancretan-forum.appspot.com',
+    messagingSenderId: '867139335721'
+  }
 };
